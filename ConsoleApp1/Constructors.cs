13efb0e7cbced819d0e039fe5b589b2353be2876
Anton Ryabuhin <Lordd871@gmail.com>
2020-08-23 20:56:05 +1000

    add props, constructors

---
+++ ConsoleApp1/Constructors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Constructors
+    {
+        private int _args { get; set; } = 100;
+        private string race;
+
+        public Constructors()
+        {
+            race = "human";
+        }
+
+        public Constructors(int args)
+        {
+            _args = args;
+            if (args > 100)
+                race = "orc";
+            race = "human";
+        }
+    }
+}
---
+++ ConsoleApp1/Properties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Properties
+    {
+        private string _field = null;
+        public string Field 
+        {
+            get 
+            {
+                return _field; 
+            } 
+
+            set
+            {
+                _field = value; 
+            }
+        }
+    }
+}
